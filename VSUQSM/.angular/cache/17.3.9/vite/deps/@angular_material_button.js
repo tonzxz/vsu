acd77b97262b4408cea5b2b6e75ae56129b1d44c
ken2 <kennethjhonaycardo@gmail.com>
2024-09-30 12:52:24 +0800

    invisible space remove

---
+++ VSUQSM/.angular/cache/17.3.9/vite/deps/@angular_material_button.js
@@ -0,0 +1,34 @@
+import {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+} from "./chunk-J2RNQIWG.js";
+import "./chunk-LKOZD3DC.js";
+import "./chunk-YBU5AEI2.js";
+import "./chunk-MBHNKMTP.js";
+import "./chunk-L27AST6M.js";
+import "./chunk-ASLTLD6L.js";
+export {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+};
+//# sourceMappingURL=@angular_material_button.js.map

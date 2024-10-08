61921e5614c28f012dd08bddaf319a9f31227da6
tonzxz <cabais1212@gmail.com>
2024-10-09 00:19:51 +0800

    palatog

--- src/app/services/admin.service.ts
+++ src/app/services/admin.service.ts
@@ -15,20 +15,29 @@ constructor(private API: UswagonCoreService) {
 async fetchUsers() {
   const data = await this.API.read({
     selectors: [
-      '*'
+      'terminals.id',             
+      'terminals.number',         
+      'desk_attendants.fullname', 
+      'desk_attendants.username', 
+      'divisions.name AS division',
+      'terminals.is_online'
     ],
-    tables: 'terminals',
-    conditions: ``
+    tables: 'terminals', 
+    conditions: `
+      LEFT JOIN desk_attendants ON terminals.desk_attendant_id = desk_attendants.id
+      LEFT JOIN divisions ON terminals.division_id = divisions.id
+    ` 
   });
   
-  if(data.success && data.output.length > 0){
-   console.log(data.output[0]);
-   for(let users of data.output){
-     console.log(users);
-   }
+  if (data.success && data.output.length > 0) {
+    for (let user of data.output) {
+      console.log('User Data:', user);  
+    
+    }
+  } else {
+    console.error('No users found or query failed');
   }
 }
-
 
   
 }
